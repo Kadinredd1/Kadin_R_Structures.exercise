bf3bc1cc245cffeb2b9ff7cffb3515092b96ddcb
Kadinredd1 <67976031+Kadinredd1@users.noreply.github.com>
2020-07-23 14:30:03 -0500

    Add files via upload

---
+++ Structures.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+struct engine {
+    var engine1 = "v4"
+    var engine2 = "v6"
+    var engine3 = "v8"
+
+}
+let engineChoice = engine()
+
+print("With the 2020 Audi A4 45 Premium quattro Sedan ypu have a choice of a \(engineChoice.engine1), \(engineChoice.engine2), \(engineChoice.engine3)", "engine")
